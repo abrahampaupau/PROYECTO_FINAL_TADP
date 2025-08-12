bc4e3f34585d674bd28ef501cba04adc65b7391f
abrahamCruz <jcruz41855@>
2025-08-11 19:03:56 -0600

    NUEVOS CAMBIOS

--- PROYECTO_FINAL_TADP/HashTableChaining.h
+++ PROYECTO_FINAL_TADP/HashTableChaining.h
@@ -1,76 +1,83 @@
 #pragma once
 
 #include <iostream>
-#include <forward_list> 
+#include <forward_list>
 #include <string>
-#include <vector> // Agregamos el vector para que la clase funcione correctamente
 using namespace std;
 
 
 template <typename T>
 class HashTableChaining
 {
+	// un lugar donde almacenar los datos
+	forward_list<T>* data;
+	unsigned int arraySize;
+
 public:
-    // Constructor
-    HashTableChaining(unsigned int size)
-    {
-        data.resize(size);
-        this->size = size;
-    }
 
-    // Hacemos que la funcion Add sea virtual, como lo pidió tu profesor.
-     // AQUI ESTA LA CORRECCION IMPORTANTE PARA EL OVERRIDE
-    virtual void Add(T element)
-    {
-        int index = HashFunction(element);
-        data[index].push_front(element);
-    }
-    
-    // AQUI ESTA LA CORRECCION IMPORTANTE PARA EL OVERRIDE
-    // Hacemos que la funcion Remove sea virtual.
-    virtual void Remove(T element)
-    {
-        int index = HashFunction(element);
-        forward_list<T>& listAtIndex = data[index];
-        listAtIndex.remove(element);
-    }
+	HashTableChaining(unsigned int size)
+	{
+		// pedimos memoria para nuestras N listas.
+		data = new forward_list<T>[size];
+		arraySize = size;
+	}
 
-    // Hacemos que la funcion Contains sea virtual.
-    virtual bool Contains(T element)
-    {
-        int index = HashFunction(element);
-        forward_list<T>& listAtIndex = data[index];
-        for (auto i : listAtIndex)
-        {
-            if (i == element)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+	// ¡Aquí agrego el destructor para liberar la memoria que pedí con 'new'!
+	
+	~HashTableChaining()
+	{
+		delete[] data;
+	}
 
-    void Print()
-    {
-        for (size_t i = 0; i < data.size(); i++)
-        {
-            cout << "lista del indice: " << i << ": ";
-            for (auto element : data[i])
-            {
-                cout << element << ", ";
-            }
-            cout << endl;
-        }
-    }
+	// Función Hash
+	int HashFunction(T key)
+	{
+		return key % arraySize;
+	}
 
-protected:
-    // Hash function
-    int HashFunction(T element)
-    {
-        hash<T> hasher;
-        return hasher(element) % size;
-    }
+	// Metodos (con las correcciones)
+	//  le agregué la palabra 'virtual' para que la herencia funcione bien.
+	virtual void Add(T element)
+	{
+		int index = HashFunction(element);
+		data[index].push_front(element);
+	}
 
-    vector<forward_list<T>> data;
-    unsigned int size;
+	// También hice 'virtual' esta función por si quiero cambiarla en el futuro.
+	virtual void Remove(T element)
+	{
+		int index = HashFunction(element);
+		forward_list<T>& listAtIndex = data[index];
+		// La función de la lista hace la búsqueda y borra el elemento por mí.
+		listAtIndex.remove(element);
+	}
+
+	// Y también hago 'virtual' la función de búsqueda.
+	virtual bool Contains(T element)
+	{
+		int index = HashFunction(element);
+		forward_list<T>& listAtIndex = data[index];
+		for (auto i : listAtIndex)
+		{
+			if (i == element)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void Print()
+	{
+		for (int i = 0; i < arraySize; i++)
+		{
+			cout << "lista del índice: " << std::to_string(i) << ": ";
+			for (auto j : data[i])
+				cout << j << ", ";
+			cout << endl;
+		}
+	}
 };
+
+
+void HashTableChainingExample();
